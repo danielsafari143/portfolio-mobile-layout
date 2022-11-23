7de0ff75a7038f60ec1a1b398969d982fe3122dc
danielsafari143 <danielsafari143@gmail.com>
2022-11-23 22:35:50 +0100

    updated html and js file

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
 const menuIconMobile = document.querySelector('.navbar__hamb');
-const menu = document.querySelector(".navbar__menu");
-const viewport = document.querySelector("body");
-const menuLinks = document.querySelectorAll(".navbar__menu__link");
+const menu = document.querySelector('.navbar__menu');
+const viewport = document.querySelector('body');
+const menuLinks = document.querySelectorAll('.navbar__menu__link');
 
 function isActiveClassExisted(element) {
-  return element.classList.contains("active");
+  return element.classList.contains('active');
 }
 
 function refreshIcon() {
   if (isActiveClassExisted(menu)) {
-    menuIconMobile.setAttribute("src", "images/hamburguer-close.png");
+    menuIconMobile.setAttribute('src', 'img/Icon.png');
+    document.querySelector('.headline').style.filter = 'blur(5px)';
   } else {
-    menuIconMobile.setAttribute("src", "images/hamburguer.png");
+    menuIconMobile.setAttribute('src', 'https://cdn-icons-png.flaticon.com/512/5107/5107718.png');
+    document.querySelector('.headline').style.filter = 'blur(0px)';
   }
 }
 function openMenu() {
-  menu.classList.toggle("active");
-  viewport.classList.add("blockover");
+  menu.classList.toggle('active');
+  viewport.classList.add('blockover');
   refreshIcon();
 }
 
 function closeMenu() {
-  menu.classList.remove("active");
-  viewport.classList.remove("blockover");
+  menu.classList.remove('active');
+  viewport.classList.remove('blockover');
   refreshIcon();
 }
 
-menuIconMobile.addEventListener("click", openMenu);
+menuIconMobile.addEventListener('click', openMenu);
 menuLinks.forEach((menuLink) => {
-  menuLink.addEventListener("click", closeMenu);
+  menuLink.addEventListener('click', closeMenu);
 });
